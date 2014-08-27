99d8df331b6076489db3803bf4f454d3019276a0
Herry <herry13@gmail.com>
2014-08-27 23:34:57 +0100

    refactor module action

--- ocaml/action.mli
+++ ocaml/action.mli
@@ -1,48 +1,67 @@
 open Common
 open Domain
 
-(** name * parameters * cost * preconditions * effects **)
-(*type t = reference * basic MapStr.t * cost * basic MapRef.t * basic MapRef.t*)
+(**
+ * This module represents a grounded SFP action.
+ *)
+
+(** type of an action **)
 type t = {
-	name: reference;
-	parameters: basic MapStr.t;
-	cost: cost;
+	name:          reference;
+	parameters:    basic MapStr.t;
+	cost:          cost;
 	preconditions: basic MapRef.t;
-	effects: basic MapRef.t
+	effects:       basic MapRef.t
 }
 
+(** type of a collection of actions **)
 type ts = { total: int; actions: t list }
 
+(** create an action **)
+val make : reference -> basic MapStr.t -> int -> basic MapRef.t ->
+           basic MapRef.t -> t
 
-val make : reference -> basic MapStr.t -> int -> basic MapRef.t -> basic MapRef.t -> t
-
+(** return given action's name **)
 val name : t -> reference
 
+(** return given action's parameters **)
 val parameters : t -> basic MapStr.t
 
+(** return given action's cost **)
 val cost : t -> int
 
+(** return given action's preconditions **)
 val preconditions : t -> basic MapRef.t
 
+(** return given action's effects **)
 val effects : t -> basic MapRef.t
 
+(** generate JSON of an action **)
 val json_of : t -> string
 
+(** generate JSON of an action with precedence constraints **)
 val json_of_parallel_action : t -> int list -> int list -> string
 
-
+(** generate JSON of a list of actions **)
 val json_of_actions : t list -> string
 
-val ground_actions : Type.env -> Variable.ts -> Type.typevalue -> _constraint -> _constraint list -> ts
+(** ground a collection of actions **)
+val ground_actions : Type.env -> Variable.ts -> Type.typevalue ->
+                     _constraint -> _constraint list -> ts
 
+(** iterates a collection of actions **)
 val iter : (t -> unit) -> ts -> unit
 
+(** folds a collection of actions **)
 val fold : ('a -> t -> 'a) -> 'a -> ts -> 'a
 
+(** add an action to the collection **)
 val add : t -> ts -> ts
 
+(** return an empty collection of actions **)
 val empty : ts
 
+(** convert a collection of actions to an array of actions **)
 val to_array : ts -> t array
 
 
